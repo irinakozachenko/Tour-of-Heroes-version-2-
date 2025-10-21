3e84abd767db54c90975b5e9a543cbdd5d542e37
irinakozachenko <irina.kozachenko@dsr-corporation.com>
2025-10-21 17:45:42 +0300

    edit key in Hero interface

--- src/app/hero.type.ts
+++ src/app/hero.type.ts
@@ -11,7 +11,7 @@ export interface Hero {
   email?: string,
   checked?: boolean,
 
-  [index: string]: string | number | Date | boolean | undefined;
+  [index: string]: any;
 }
 
 export interface PopupModel {
